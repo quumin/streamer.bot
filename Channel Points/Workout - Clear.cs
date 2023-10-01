e79c010148520428b26a3c4dcb09f262dcf94907
drdankenstein <engineeringacause@gmail.com>
2023-10-01 23:51:13 +0200

    Fixed the workout counter to work using the User Group setting.

--- Channel Points/Workout - Clear.cs
+++ Channel Points/Workout - Clear.cs
@@ -18,21 +18,26 @@ public class CPHInline
         //Initializations 
         str_path = new string[]
         {
-            "W:\\Streaming\\Exercise.txt",
-            "W:\\Streaming\\Exercise.csv"
+            @".\\external_files\\Exercise.txt",
+            @".\\external_files\\Exercise.csv"
         };
+
+        CPH.UnsetGlobalVar("deathCounter");
 
         //Clear .txt
         fs_open = File.Open(str_path[0], FileMode.Open);
         fs_open.SetLength(0);
         fs_open.Close();
-        
+
         //Clear .csv
         fs_open = File.Open(str_path[1], FileMode.Open);
         fs_open.SetLength(0);
         fs_open.Close();
 
         CPH.SendMessage("/me Workout Files cleared Q-Mander NODDERS");
+
+        CPH.DisableCommand("69e5a7a8-8d90-4064-b8dd-68a7a63268d1");
+        CPH.DisableCommand("d3422d01-3041-49ac-bed8-1d72c9d12dec");
 
         return true;
     }//Execute()
--- Channel Points/Workout.cs
+++ Channel Points/Workout.cs
@@ -14,15 +14,15 @@ public class CPHInline
         //Declarations
         string[] str_meta, str_src, str_path, str_cond, str_format, str_reward;
         int[] int_cnt;
-        string str_ss;
+        string str_ss, str_d;
         int int_wait;
         float f_vol;
 
         //Initializations 
         str_meta = new string[]
         {
-            args["user"].ToString(),
-            args["rawInput"].ToString(),
+            "",
+            "",
             args["__source"].ToString()
         };
         str_src = new string[]
@@ -33,10 +33,10 @@ public class CPHInline
         str_path = new string[]
         {
             $"{CPH.GetGlobalVar<string>("mediaRoot")}LetsGetSalty.mp3",
-            "W:\\Streaming\\Exercise.txt",
-            "W:\\Streaming\\Exercise.csv"
+            @".\\external_files\\Exercise.txt",
+            @".\\external_files\\Exercise.csv"
         };
-        str_cond = str_meta[1].Split(new string[] { " |d| " }, StringSplitOptions.None);
+        str_d = " |d| ";
         str_format = new string[2];
         str_reward = new string[3];
         int_cnt = new int[2];
@@ -60,12 +60,26 @@ public class CPHInline
                     Convert.ToInt32(args["counter"].ToString()),
                     Convert.ToInt32(args["userCounter"].ToString())
                 };
+                str_meta = new string[]
+                {
+                    args["user"].ToString(),
+                    args["rawInput"].ToString(),
+                    args["__source"].ToString()
+                };
                 break;
             //  Testing
+            case "HotKeyPress":
             case "CommandMessage":
                 str_reward = new string[]
                 {
-                    "rewardID",
+                    /*Reward ID's
+                     * Non Death-Counter:
+                     *  f3907ebe-0011-4fe4-b85c-7d94101e5f33 
+                     * 
+                     * Death Counter:
+                     *  0d8161d7-1689-49a3-97ca-f5769ecb1b75
+                     */
+                    "0d8161d7-1689-49a3-97ca-f5769ecb1b75",
                     "LIFT!",
                     "redeemID"
                 };
@@ -74,14 +88,23 @@ public class CPHInline
                     1,
                     2
                 };
+                str_meta = new string[]
+                {
+                    "quumin",
+                    $"This is a test{str_d}5",
+                    args["__source"].ToString()
+                };
+                CPH.SendMessage("Testing Workout dataMask", true);
                 break;
             default:
                 return true;
                 break;
         }//switch
 
+        str_cond = str_meta[1].Split(new string[] { str_d }, StringSplitOptions.None);
+
         //If the delimiter was used incorrectly...
-        if (str_cond.Length > 2)
+        if ((str_cond.Length > 2) && (str_reward[0] == "0d8161d7-1689-49a3-97ca-f5769ecb1b75"))
         {
             CPH.SendMessage($"/me {str_meta[0]} you used the delimiter too many times... disGUSTING ");
             //... and if I'm live...
@@ -93,7 +116,7 @@ public class CPHInline
             return true;
         }//if
          //If the delimiter was used correctly...
-        else if (str_cond.Length == 2)
+        else if ((str_cond.Length == 2) && (str_reward[0] == "0d8161d7-1689-49a3-97ca-f5769ecb1b75"))
         {
             //... prompt user & enable death counter.
             CPH.SendMessage($"/me @{str_meta[0]} - Death Counter is enabled! Use \'!died\' to increase your counter! WICKED");
@@ -103,6 +126,12 @@ public class CPHInline
                 $"[_]: {str_meta[0]} [{int_cnt[1]}]/[{int_cnt[0]}] - \'{str_cond[0]}\' ({str_cond[1]})",
                 $"{str_meta[0]};{int_cnt[1]};{int_cnt[0]};{str_cond[0]};{str_cond[1]};{str_reward[2]}"
             };
+            CPH.DisableReward(str_reward[0]);
+            CPH.EnableCommand("9b23991e-ac4b-48bf-afc4-bce82ad3d674");
+            CPH.EnableCommand("69e5a7a8-8d90-4064-b8dd-68a7a63268d1");
+            CPH.EnableCommand("d3422d01-3041-49ac-bed8-1d72c9d12dec");
+            CPH.EnableCommand("567a1801-dd6e-4a08-9639-e250a89403ba");
+            CPH.AddUserToGroup(str_meta[0], "WorkOut Starter");
         }//else if
         else
         {
